4179464df8c4ad9a76e8802e61f7648acf90021c
Kelvin Graddick <development@wavelinkllc.com>
2017-07-04 02:41:17 -0400

    Removed All Game Hunting demo

--- allgamehunting/css/main.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<link href='http://<?php echo $_SERVER['SERVER_NAME']; ?>/allgamehunting/css/main.css' rel='stylesheet' type='text/css'>
-<link href='http://<?php echo $_SERVER['SERVER_NAME']; ?>/allgamehunting/css/lightbox.css' rel='stylesheet' type='text/css'>
-<link rel="shortcut icon" href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/favicon.ico" type="image/x-icon">
-<link rel="icon" href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/favicon.ico" type="image/x-icon">
--- allgamehunting/js/main.php
+++
@@ -1,6 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
-<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/additional-methods.min.js"></script>
-<script src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/js/main.js"></script>
-<script src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/js/lightbox.js"></script>
