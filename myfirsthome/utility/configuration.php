98e7569910c9c9af0a400ba090558c0a3168e9bc
Kelvin <kgraddick@wavelinkllc.com>
2017-07-16 14:40:02 -0400

    removing myfirsthome mock

--- myfirsthome/utility/configuration.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$site_name = "My First Home"; // Website name
-	$site_image = "/images/logo-social-yellow-big.png"; // Website image
-
-	$database_host = "localhost"; // Mysql host name
-	$database_username = "application"; // Mysql username
-	$database_password = "wavelink2014"; // Mysql password
-	$database_name = "myfirsthome"; // Mysql database name
-?>
