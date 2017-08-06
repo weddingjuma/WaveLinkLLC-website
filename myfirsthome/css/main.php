5b85858bcd0fca959ae361e5330e4b3c4e121d85
Kelvin Graddick <development@wavelinkllc.com>
2017-08-05 22:53:17 -0400

    deleted myfirsthome website mock

--- myfirsthome/css/main.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,600" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700" rel="stylesheet">
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-<link href='http://<?php echo $_SERVER['SERVER_NAME']; ?>/myfirsthome/css/main.css' rel='stylesheet' type='text/css'>
-<link rel="shortcut icon" href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/myfirsthome/favicon.ico" type="image/x-icon">
-<link rel="icon" href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/myfirsthome/favicon.ico" type="image/x-icon">
